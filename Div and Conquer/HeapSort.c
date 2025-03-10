7d64cd9c89211e05d900d996e9cd01782337ebd0
codingdaddy-ae <arghadeeptambuli05@gmail.com>
2025-03-10 23:44:18 +0530

    second commit

--- Div and Conquer/HeapSort.c
+++ Div and Conquer/HeapSort.c
@@ -20,16 +20,48 @@ void heapify(int arr[], int n, int i)
 
     if (min != i)
     {
-        swap(arr[min], arr[i]);
+        swap(&arr[min], &arr[i]);
+        heapify(arr, n, min);
     }
-
-    heapify(arr, n, min);
 }
 
-void heapsort(int arr[], int n)
+void heapSort(int arr[], int n)
 {
-    for (int i = n/2-1; i>=0; i--)
+    for (int i = (n / 2) - 1; i >= 0; i--)
     {
-        heapify(arr, n, i) //building heap rearrange array (n/2-1 signifies internal nodes as all internal nodes are )
+        heapify(arr, n, i); // building heap rearrange array (n/2-1 signifies internal nodes as all internal nodes are from 0 to n/2-1)
     }
+    for (int i = n-1; i > 0; i--)
+    {
+        swap(&arr[i], &arr[0]);
+        heapify(arr, i, 0);
+    }
+}
+
+int main()
+{
+    int n;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+    int arr[n];
+    clock_t start, end;
+    double total_t;
+
+    printf("Enter the elements of the array: ");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    start = clock();
+    heapSort(arr, n);
+    end = clock();
+
+    printf("The sorted array: ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    total_t = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("\nTime required %f\n", total_t);
 }
